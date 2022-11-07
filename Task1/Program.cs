60313fc3b7798681a5a3303b21f682e13507fb6f
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-07 18:53:48 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        //Два треугольника заданы длинами своих сторон.
+        //Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).
+        //Для решения задачи можно использовать формулу Герона 
+
+        static void GetArea(double a, double b, double c, out double s)
+        {
+            double p = (a + b + c) / 2;
+            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника 1:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            GetArea(x1, y1, z1, out double s1);
+
+            Console.WriteLine("Введите стороны треугольника 2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            GetArea(x2, y2, z2, out double s2);
+
+            if (s1 < s2)
+                Console.WriteLine("Площадь 1 треугольника меньше");
+            else if (s1 > s2)
+                Console.WriteLine("Площадь 1 треугольника больше");
+            else 
+                Console.WriteLine("Площади треугольников равны");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        //Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+        static void GetParams(double a, out double s, out double v)
+        {
+            s = 6 * (a * a);
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            GetParams(a, out double s, out double v);
+            Console.WriteLine("Площадь куба = {0:.00}; объем куба = {1:.00}.", s, v);
+            Console.ReadLine();
+        }
+    }
+}
